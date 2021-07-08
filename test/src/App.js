2003a92bb341685eff136b04dea8af98d836dc2d
unknown <yamila.sleiman01@gmail.com>
2021-07-08 14:50:37 -0300

    cart

--- test/src/App.js
+++ test/src/App.js
@@ -1,11 +1,16 @@
 import './Styles/App.scss';
 import NavBar from './components/NavBar';
+import CartWidget from './components/CartWidget';
+import ContentBuy from './components/ContentBuy';
 
 
 function App() {
+  const miNombre = "Yamila";
   return (
     <div className='container'>
-      <NavBar/>
+      <NavBar name={miNombre} />
+      <CartWidget />
+      <ContentBuy name={miNombre}/>
     </div>
   );
 }
--- test/src/components/NavBar/index.js
+++ test/src/components/NavBar/index.js
@@ -1,15 +1,24 @@
-import react from 'react';
+import React, { Component } from 'react';
 
 
-export default function NavBar(){
-    return(
-        <div className='nb_container'>
-            <label>YAMILA</label>
-            <ul className='menu'>
-                <li><a href="">Inicio</a></li>
-                <li><a href="">Comprar</a></li>
-                <li><a href="">Contacto</a></li>
-            </ul>
-        </div>
-    )
+export default class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+        
+        console.log(props);
+    }
+
+    render() {
+        return(
+            <div className='nb_container'>
+                <label>YAMILA</label>
+                <ul className='menu'>
+                    <li><a href="">Inicio</a></li>
+                    <li><a href="">Comprar</a></li>
+                    <li><a href="">Contacto</a></li>
+                </ul>
+            </div>
+        )
+    }
 };
